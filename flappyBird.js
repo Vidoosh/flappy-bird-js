016ed17e7add88c394909bcc655dccc03653bdf9
Vidoosh <82228675+Vidoosh@users.noreply.github.com>
2022-09-30 21:49:52 +0530

    Update flappyBird.js

--- flappyBird.js
+++ flappyBird.js
@@ -41,7 +41,7 @@ scor.src = "sounds/score.mp3";
 document.addEventListener("keydown",moveUp);
 
 function moveUp(){
-    bY -= 25;
+    bY -= 30;
     fly.play();
 }
 
@@ -78,7 +78,7 @@ function draw(){
 
         // detect collision
         
-        if( bX + bird.width >= pipe[i].x && bX <= pipe[i].x + pipeNorth.width && (bY <= pipe[i].y + pipeNorth.height || bY+bird.height >= pipe[i].y+constant) || bY + bird.height >=  cvs.height - fg.height){
+        if( bX + bird.width >= pipe[i].x && bX <= pipe[i].x + pipeNorth.width && (bY <= pipe[i].y + pipeNorth.height || bY + bird.height >= pipe[i].y+constant) || bY + bird.height >=  cvs.height - fg.height || bY == 0){
             location.reload(); // reload the page
         }
         
