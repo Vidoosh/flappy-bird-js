dfae3daf0ed41199228d80bb10dda5795d71d801
Vidoosh <82228675+Vidoosh@users.noreply.github.com>
2022-09-30 22:12:04 +0530

    Update flappyBird.js

--- flappyBird.js
+++ flappyBird.js
@@ -81,9 +81,6 @@ function draw(){
         if( bX + bird.width >= pipe[i].x && bX <= pipe[i].x + pipeNorth.width && (bY <= pipe[i].y + pipeNorth.height || bY + bird.height >= pipe[i].y+constant) || bY + bird.height >=  cvs.height - fg.height){
             location.reload(); // reload the page
         }
-        if(bY>200 || bY<0){
-            location.reload();
-        }
         
         if(pipe[i].x == 5){
             score++;
